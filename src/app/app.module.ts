f5b9ff2b8910c095e12c18f609d47670b0bab64e
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-06-24 20:12:26 -0300

    router module with root and error path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,33 @@ import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './navbar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { ErrorComponent } from './error-404/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: '**', component: ErrorComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
